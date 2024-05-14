57030718ebeb464b475f4f196b99831dd196827b
Richard <programmingislife23@gmail.com>
2024-05-14 19:53:09 -0300

    first commit

---
+++ quickstart/src/main/java/com/devtiro/quickstart/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.devtiro.quickstart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloWorldController {
+    
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Helloo Dev";
+    }
+}
